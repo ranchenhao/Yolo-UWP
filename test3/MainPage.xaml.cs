d35d92c530597cc723194c0c09d3520c7a3e5036
Ran <ranchenhao@qq.com>
2020-09-08 02:02:08 +0100

    Initial commit

---
+++ test3/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Shapes;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace test3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.objectDetection = new ObjectDetection();
+        }
+
+        public ObjectDetection objectDetection;
+
+        public async void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            StorageFile storageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/test.jpg"));
+            SoftwareBitmap softwareBitmap;
+
+            using (IRandomAccessStream stream = await storageFile.OpenAsync(FileAccessMode.Read))
+            {
+                // Create the decoder from the stream
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                // Get the SoftwareBitmap representation of the file
+                softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+            }
+
+            // Draw original image
+            Image img = sender as Image;
+            img.Source = new BitmapImage(new Uri("ms-appx:///Assets/test.jpg"));
+
+            // Predict image
+            await objectDetection.LoadModelAsync();
+            var boundingboxes = await objectDetection.PredictImageAsync(softwareBitmap);
+
+            // Draw bounding box
+            var originalImageWidth = softwareBitmap.PixelWidth;
+            var originalImageHeight = softwareBitmap.PixelHeight;
+
+            foreach (var box in boundingboxes)
+            {
+                var x = (uint)Math.Max(box.Dimensions.X, 0);
+                var y = (uint)Math.Max(box.Dimensions.Y, 0);
+                var width = (uint)Math.Min(originalImageWidth - x, box.Dimensions.Width);
+                var height = (uint)Math.Min(originalImageHeight - y, box.Dimensions.Height);
+
+                x = (uint)originalImageWidth * x / 416;
+                y = (uint)originalImageHeight * y / 416;
+                width = (uint)originalImageWidth * width / 416;
+                height = (uint)originalImageHeight * height / 416;
+
+                TextBlock text = new TextBlock();
+                text.Text = $"{box.Label} ({(box.Confidence * 100).ToString("0")}%)";
+                text.FontSize = 20;
+                text.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                canvas.Children.Add(text);
+                Canvas.SetLeft(text, x);
+                Canvas.SetTop(text, y - 25);
+
+                Rectangle rect = new Rectangle();
+                rect.Width = width;
+                rect.Height = height;
+                rect.Fill = new SolidColorBrush(Windows.UI.Colors.Blue);
+                rect.Fill.Opacity = 0;
+                rect.Stroke = new SolidColorBrush(Windows.UI.Colors.Black);
+                rect.StrokeThickness = 3;
+
+                canvas.Children.Add(rect);
+                Canvas.SetTop(rect, y);
+                Canvas.SetLeft(rect, x);
+            }
+        }
+    }
+}
---
+++ test3/ObjectDetection.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Media;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.AI.MachineLearning;
+using YoloV2;
+
+public class ObjectDetection
+{
+    public string info_text;
+
+    private Model model = null;
+    private string ModelFilename = "tinyyolov2-8.onnx";
+    private Stopwatch TimeRecorder = new Stopwatch();
+
+    public async Task LoadModelAsync()
+    {
+        ModifyText($"Loading {ModelFilename}... Patience");
+
+        try
+        {
+            TimeRecorder = Stopwatch.StartNew();
+
+            var modelFile = await StorageFile.GetFileFromApplicationUriAsync(
+                new Uri($"ms-appx:///Assets/{ModelFilename}"));
+            model = await Model.CreateFromStreamAsync(modelFile);
+
+            TimeRecorder.Stop();
+            ModifyText($"Loaded {ModelFilename}: Elapsed time: {TimeRecorder.ElapsedMilliseconds} ms");
+        }
+        catch (Exception ex)
+        {
+            ModifyText($"error: {ex.Message}");
+            model = null;
+        }
+    }
+
+    public async Task<IList<YoloBoundingBox>> PredictImageAsync(SoftwareBitmap sfbmp)
+    {
+        IList<YoloBoundingBox> result = null;
+        if (sfbmp != null)
+        {
+            try
+            {
+                TimeRecorder.Restart();
+
+                Input inputData = new Input();
+                await PreProcess(sfbmp, inputData);
+                var output = await model.EvaluateAsync(inputData).ConfigureAwait(false);
+                YoloOutputParser parser = new YoloOutputParser();
+                var boundingBoxes = parser.ParseOutputs(output.grid.GetAsVectorView());
+                result = parser.FilterBoundingBoxes(boundingBoxes, 5, .5F);
+
+                TimeRecorder.Stop();
+
+                string message = $"({DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second})" +
+                    $" Evaluation took {TimeRecorder.ElapsedMilliseconds}ms\n";
+
+                message = message.Replace("\\n", "\n");
+
+                ModifyText(message);
+            }
+            catch (Exception ex)
+            {
+                var err_message = $"error: {ex.Message}";
+                ModifyText(err_message);
+            }
+        }
+        return result;
+    }
+
+    private void ModifyText(string text)
+    {
+        System.Diagnostics.Debug.WriteLine(text);
+        info_text = text;
+    }
+
+    private async Task PreProcess(SoftwareBitmap sfbmp, Input input)
+    {
+        int iw = sfbmp.PixelWidth;
+        int ih = sfbmp.PixelHeight;
+        uint w, h;
+        w = h = 416; // target image size
+        SoftwareBitmap resizedBitMap;
+
+        // Resize image
+        using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+        {
+            BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, stream);
+
+            encoder.SetSoftwareBitmap(sfbmp);
+
+            encoder.BitmapTransform.ScaledWidth = w;
+            encoder.BitmapTransform.ScaledHeight = h;
+            encoder.BitmapTransform.InterpolationMode = BitmapInterpolationMode.Cubic;
+
+            await encoder.FlushAsync();
+
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+            resizedBitMap = await decoder.GetSoftwareBitmapAsync(sfbmp.BitmapPixelFormat, sfbmp.BitmapAlphaMode);
+        }
+        VideoFrame inputImage = VideoFrame.CreateWithSoftwareBitmap(resizedBitMap);
+        input.image = ImageFeatureValue.CreateFromVideoFrame(inputImage);
+        
+        /*
+        input.input_1 = inputImage;
+        input.image_shape = new float[1, 2];
+        input.image_shape[0, 0] = ih;
+        input.image_shape[0, 1] = iw;
+        */
+    }
+}
